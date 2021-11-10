baf1a6e268cf84ee900f0675e9600989f6c34088
Guneet Rekhi <rekhi.guneet@gmail.com>
2021-11-09 16:37:47 -0800

    bug fixed missing function name

--- validation/register.js
+++ validation/register.js
@@ -1,7 +1,7 @@
 const Validator = require('validator');
 const isEmpty = require('./is-empty');
 
-module.exports = function (data){
+module.exports = function validateRegisterInput(data){
 
     let errors = {}; //to collect errors
 
