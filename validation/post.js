32b0584ef41f46cc00bde134c0c1c726855e2a5b
Guneet Rekhi <rekhi.guneet@gmail.com>
2021-11-18 13:45:44 -0800

    Image field is required

--- validation/post.js
+++ validation/post.js
@@ -4,12 +4,8 @@ const isEmpty = require("./is-empty");
 module.exports = function validatePostInput(data) {
   let errors = {};
 
-  if (!Validator.isLength(data.text, { min: 2, max: 300 })) {
-    errors.text = "Post must be between 2 and 300 characters";
-  }
-
-  if (isEmpty(data.text)) {
-    errors.text = "Text field is required";
+  if (isEmpty(data.image)) {
+    errors.image = "Image field is required";
   }
 
   return {
