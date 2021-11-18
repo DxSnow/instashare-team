a785d91e43ade294b1bc1da5329da5c55d43df30
Guneet Rekhi <rekhi.guneet@gmail.com>
2021-11-18 14:12:47 -0800

    updated validation code

--- validation/post.js
+++ validation/post.js
@@ -4,8 +4,12 @@ const isEmpty = require("./is-empty");
 module.exports = function validatePostInput(data) {
   let errors = {};
 
-  if (isEmpty(data.image)) {
-    errors.image = "Image field is required";
+  if (!Validator.isLength(data.text, { min: 2, max: 300 })) {
+    errors.text = "Text must be between 2 and 300 characters";
+  }
+
+  if (isEmpty(data.text)) {
+    errors.text = "Text field is required";
   }
 
   return {
