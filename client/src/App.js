54017019a261260e86b4c335a2975a0088852551
Guneet Rekhi <rekhi.guneet@gmail.com>
2021-12-02 11:24:46 -0800

    Integrated UI for Register a user

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,29 @@
-import './App.css';
+//importing libraries
 import React, { Component} from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+
+//importing css and components
+import './App.css';
 import Header from './components/layout/Header';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
+import Register from './components/auth/Register';
+ 
 
-
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
       <Router>
         <div className = "App">
           <Header />
-
           <Routes>
               <Route exact path="/" element={<Landing />} />
-
+              <Route exact path="/register" element={<Register />} />
           </Routes>
-
           <Footer />
-
-
         </div>
       </Router>
     )
   }
 }
+export default App;
--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -4,7 +4,7 @@ export default class Header extends Component {
   render() {
     return (
       <header>
-        <h1 appName>Instashare</h1>
+        <h1>Instashare</h1>
         {/* <nav>
           <ul>
             <li>Login</li>
