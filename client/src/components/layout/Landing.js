555d9f4087e6ec4b2218c923961476cb3062922b
DxSnow <62160409+DxSnow@users.noreply.github.com>
2021-12-03 10:58:53 -0800

    add log in component with state,axios,error alert

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 
 export default class Landing extends Component {
   render() {
     return (
-      <div>
+      <div className="landing">
         <button to = "/" className = "button demo"><span>Demo Login</span></button>
         <br />
-        <button  className="button register"><span>Sign Up</span></button>
+
+        <Link to="/register" className="button register"><span>Sign Up</span></Link>
         <br />
-        <Link to="/register" className="button register" style={{ textDecoration: 'none' }}>Sign Up</Link>
+        <Link to="/login" className="button login"><span>Log In</span></Link>
         <br />
-        <button  className="button login"><span>Log In</span></button>
 
       </div>
     )
