224f9ea7b406943341889c219f1d7c0f674d71c4
DxSnow <62160409+DxSnow@users.noreply.github.com>
2022-02-16 18:06:22 -0800

    excluded redux-devtools-extension from production to fix blank page issue

--- client/src/store.js
+++ client/src/store.js
@@ -3,12 +3,27 @@ import thunk from 'redux-thunk'; // breaking data into chunks
 import rootReducer from './reducers';// will refer to index.js file
 
 const middleware = [thunk];
-const store = createStore(
-              rootReducer, // list of all reducers 
-              {}, 
-              compose( 
-                applyMiddleware(...middleware),
-                 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-              ));
+const initialState = {}
+
+// the following code is disabled because if a browser does not have redux_devtools installed it will show blank page.
+// const store = createStore(
+//               rootReducer, // list of all reducers
+//               {},
+//               compose(
+//                 applyMiddleware(...middleware),
+//                  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//               ));
+
+//  to solve above problem, we can exclude redux_devtools when in production
+if(process.env.NODE_ENV === 'production') {
+  store = createStore(rootReducer, initialState, compose(
+      applyMiddleware(...middleware)
+  ));
+} else {
+  store = createStore(rootReducer, initialState, compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  ));
+}
 
 export default store;
