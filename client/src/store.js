6bad1516b722ff50268ab9a8d8012d0092c49467
DxSnow <62160409+DxSnow@users.noreply.github.com>
2022-02-16 19:11:15 -0800

    fixed safari blank page issue

--- client/src/store.js
+++ client/src/store.js
@@ -3,12 +3,14 @@ import thunk from 'redux-thunk'; // breaking data into chunks
 import rootReducer from './reducers';// will refer to index.js file
 
 const middleware = [thunk];
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-              rootReducer, // list of all reducers 
-              {}, 
-              compose( 
-                applyMiddleware(...middleware),
-                 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+              rootReducer, // list of all reducers
+              {},
+              composeEnhancers(
+                applyMiddleware(...middleware)
               ));
+
 
 export default store;
