fa163561d11b5f485add247290b72f0c7d99d0c2
DxSnow <62160409+DxSnow@users.noreply.github.com>
2022-04-11 15:57:33 -0700

    modified JWT payload

--- routes/api/users.js
+++ routes/api/users.js
@@ -87,7 +87,7 @@ router.post('/login', (req, res) => {
         }else{
           //generate token
           const payload = {
-            id: user.id // avoid PII
+            id: user.id // avoid PII(Personally identifiable information)
           };
           jwt.sign(
             payload ,
